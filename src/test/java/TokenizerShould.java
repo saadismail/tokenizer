49cb513b61cae1db8e366edd5f4563ed82eb7283
Saad Ismail <saad3112@gmail.com>
2020-05-08 15:01:32 +0500

    initial project structure

---
+++ src/test/java/TokenizerShould.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TokenizerShould {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(true, true);
+    }
+}
