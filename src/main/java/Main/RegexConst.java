c24b0e2d9a32fbb3a8461c5227af8b881b804d68
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-05-08 22:28:44 +0500

    REL_OP work started

--- src/main/java/Main/RegexConst.java
+++ src/main/java/Main/RegexConst.java
@@ -6,9 +6,13 @@ public class RegexConst {
     public static final String INTEGER_CONST = "\\d+";
     public static final String REAL_CONST = "\\d+.\\d+";
     public static final String STRING_CONST = "[\"“].*[\"”]";
+    public static final String REL_OP = "<>|<=|>=|=|<|>";
+    public static final String OP = "+|-|*|/";
+
+    public static final String EXPR = "[[" + OP + "]|[" + REL_OP + "]]+";
 
     private static final String VARIABLE_NAME_OR_INT_CONST_OR_REAL_CONST = VARIABLE_NAME + "|" + INTEGER_CONST + "|" + REAL_CONST;
-    private static final String STRING_CONST_OR_VARIABLE_NAME = VARIABLE_NAME + "|" + STRING_CONST;
+    private static final String VARIABLE_NAME_OR_STRING_CONST = VARIABLE_NAME + "|" + STRING_CONST;
 
     public static final String PROGRAM_LINE = "(program)\\s+("+ VARIABLE_NAME +")\\s*(;)";
     public static final String VAR_LINE = "\\s*(var)\\s*";
@@ -17,5 +21,5 @@ public class RegexConst {
     public static final String END_LINE = "\\s*(end)(.)?\\s*";
     public static final String ASSIGNMENT_LINE = "\\s*(" + VARIABLE_NAME + ")\\s*(:=)\\s*(" + VARIABLE_NAME_OR_INT_CONST_OR_REAL_CONST + ")\\s*(;)";
     public static final String READLN_LINE = "\\s*(readln)\\s*(\\()("+VARIABLE_NAME+")\\s*(\\))\\s*(;)";
-    public static final String WRITE_LINE = "\\s*(write|writeln)\\s*(\\()("+STRING_CONST_OR_VARIABLE_NAME+")\\s*(\\))\\s*(;)";
+    public static final String WRITE_LINE = "\\s*(write|writeln)\\s*(\\()("+ VARIABLE_NAME_OR_STRING_CONST +")\\s*(\\))\\s*(;)";
 }
