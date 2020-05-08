7720be2a47df23de0573458e0672d9ff0f1814ad
Saad Ismail <saad3112@gmail.com>
2020-05-08 16:05:10 +0500

    program line parse

--- src/main/java/Tokenizer.java
+++ src/main/java/Tokenizer.java
@@ -1,23 +1,53 @@
 import Model.Symbol;
 import Model.Token;
+import Model.TokenType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static Model.TokenType.*;
 
 public class Tokenizer {
-    String code;
+    List<Token> tokens;
+    List<Symbol> symbols;
 
     public void tokenize(String code) {
-        this.code = code;
+        this.tokens = new ArrayList<>();
+        this.symbols = new ArrayList<>();
+
+        List<String> lines = Arrays.asList(code.split("\n"));
+
+        for (int idx = 0; idx < lines.size(); idx++) {
+            int lineNumber = idx + 1;
+            String line = lines.get(idx);
+
+            if (line.startsWith("program")) {
+                Pattern pattern = Pattern.compile("(program)\\s+([\\w_][_\\w\\d]*)\\s*(;)");
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    this.tokens.add(new Token(KEYWORD, matcher.group(1), lineNumber, getMatcherStartingIndex(matcher, 1)));
+
+                    this.tokens.add(new Token(IDENTIFIER, matcher.group(2), lineNumber, getMatcherStartingIndex(matcher, 2)));
+                    this.symbols.add(new Symbol(matcher.group(2), IDENTIFIER));
+
+                    this.tokens.add(new Token(SYMBOL, matcher.group(3), lineNumber, getMatcherStartingIndex(matcher, 3)));
+                }
+            }
+        }
+    }
+
+    private int getMatcherStartingIndex(Matcher matcher, int groupId) {
+        return matcher.start(groupId) + 1;
     }
 
     public List<Token> getTokenList() {
-        List<Token> tokens = new ArrayList<>();
-        return tokens;
+        return this.tokens;
     }
 
     public List<Symbol> getSymbolTable() {
-        List<Symbol> symbols = new ArrayList<>();
-        return symbols;
+        return this.symbols;
     }
 }
--- src/test/java/TokenizerTest.java
+++ src/test/java/TokenizerTest.java
@@ -1,11 +1,13 @@
 import Model.Symbol;
 import Model.Token;
+import Model.TokenType.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static Model.TokenType.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
@@ -20,13 +22,15 @@ public class TokenizerTest {
 
 
         List<Token> actualToken = Arrays.asList(
-
+            new Token(KEYWORD, "program", 1, 1),
+            new Token(IDENTIFIER, "checkMyAbility", 1, 9),
+            new Token(SYMBOL, ";", 1, 23)
         );
         List<Token> tokenList = tokenizer.getTokenList();
         assertEquals(actualToken, tokenList);
 
-        List<Token> actualSymbols = Arrays.asList(
-
+        List<Symbol> actualSymbols = Arrays.asList(
+            new Symbol("checkMyAbility", IDENTIFIER)
         );
         List<Symbol> symbols = tokenizer.getSymbolTable();
         assertEquals(actualSymbols, symbols);
